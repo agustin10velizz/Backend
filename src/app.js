82fff630dc0f10b261065db4c1134f799b5a8a13
agustin <agustin10veliz@hotmail.com>
2022-11-08 19:47:28 -0300

    desafio 5

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
 import express from 'express';
 import ProductosRouter from './routes/productos.router.js';
 import __dirname from './utils.js';
+import Manager from './Manager/Manager.js';
 
 const app = express(); //inciar el aplicativo
 const server = app.listen(8080,()=>console.log("Se creo la pagina")) //Poner al aplicativo a escuchar
 
 
-
-
 app.use(express.json())
 app.use(express.static(__dirname+'/public'))
+app.use(express.urlencoded({extended:true}))
+
 app.use("/api/productos", ProductosRouter)
 
+app.set("views",`${__dirname}/views`);
+app.set('view engine', 'ejs');
 
 
+app.get("/",(req,res)=>{
+    res.render("formulario")
+})
+
+const productoServicio = new Manager();
+app.get("/productos", async(req,res)=>{
+    const productosArray = await productoServicio.getAll()
+    res.render("history",{productosArray})
+})
--- src/routes/productos.router.js
+++ src/routes/productos.router.js
@@ -2,12 +2,12 @@ import { Router } from "express";
 import uploader from "../services/upload.js";
 import productoManager from "../Manager/Manager.js"
 
-const productoService = new productoManager();
+const productoServicio = new productoManager();
 const router = Router();
 
 
 router.get('/',async (req,res)=>{
-    let result = await productoService.getAll()
+    let result = await productoServicio.getAll()
     res.send(result);
 })
 
@@ -15,32 +15,32 @@ router.get('/',async (req,res)=>{
 router.get("/:id",async(req,res)=>{
     let id = req.params.id
     if(isNaN(id)) return res.status(400).send ({status:"error", error:"producto no encontrado"})
-    let result = await productoService.getByID(id)
+    let result = await productoServicio.getByID(id)
     res.send(result)
 })
 
 
 
-router.post("/",uploader.single("img"),async(req,res)=>{
-    const img = req.protocol+"://"+req.hostname+':8080/img'+req.file.filename
+router.post("/:id",uploader.single("img"),async(req,res)=>{
+    const img = req.protocol+"://"+req.hostname+':8080/image'+req.file.filename
     let producto = req.body;
     producto.img = img;
     producto.precio = parseInt(producto.precio);
-    const result = await productoService.save(producto);
+    const result = await productoServicio.save(producto);
     res.send({status:"bien hecho", message:"prodcuto agregado"});
 })
 
 router.put("/:id",async(req,res)=>{
    let {id} = req.params;
    let producto = req.body;
-   await productoService.putFile(producto, id)
+   await productoServicio.putFile(producto, id)
    res.send("producto actualizado")
 })
 
 router.delete("/:id",async(req,res)=>{
    let id = req.params.id;
    if(isNaN(id)) return res.status(400).send({status:"error", error:"Invalido type"})
-   let result = await productoService.deleteById(id)
+   let result = await productoServicio.deleteById(id)
        res.send(result)
 })
 
